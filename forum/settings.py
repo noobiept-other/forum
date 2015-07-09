712bc8c85b4aeaa8fa108fbe7bf05800cd475522
Pedro Ferreira <plferreira4@gmail.com>
2015-07-09 12:50:39 +0100

    - add some missing deployment stuff - fix, only a staff member can change category related stuff (in index template) - increase the thread title/text preview in the sub-forum template

--- forum/settings.py
+++ forum/settings.py
@@ -163,3 +163,13 @@ EMOTES = {
     ':|': STATIC_URL + 'emotes/unpleased.png',
     ';)': STATIC_URL + 'emotes/wink.png',
 }
+
+
+if not DEBUG:
+
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES[ 'default' ] =  dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ( 'HTTP_X_FORWARDED_PROTO', 'https' )
